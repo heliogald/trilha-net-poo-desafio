6c5ea8c3f7df2370b7c74537159fd77f6b168a3a
Hélio Galdino da Silva <heliogald@hotmail.com>
2025-10-21 16:08:20 -0300

    Implementando códigos do desafio.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,17 +3,21 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Realizando ligação...");
         }
 
         public void ReceberLigacao()
@@ -21,6 +25,7 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
+        // Método abstrato (cada marca implementa de forma diferente)
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Instanciando Nokia
+Smartphone nokia = new Nokia(numero: "987654321", modelo: "Nokia 3310", imei: "111222333", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("----------------------");
+
+// Instanciando iPhone
+Smartphone iphone = new Iphone(numero: "123456789", modelo: "iPhone 13", imei: "999888777", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
